aa300bc7fee6f600d65126948805bf429429bebf
Евгений <skytrash@Mac.local>
2025-07-23 22:14:09 +0400

    fix bag

--- NatNet/vite.config.js
+++ NatNet/vite.config.js
@@ -1,22 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'public')], // Указываем папку public
-      symbolId: 'icon-[name]', // Формат id: icon-chevron-left
-    }),
+    svgLoader()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
