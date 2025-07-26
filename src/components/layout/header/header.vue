4664d37289da99074b62de1482d5af6cbab6f72c
sky-trash <skytrash118@gmail.com>
2025-07-26 23:13:06 +0400

    full

--- src/components/layout/header/header.vue
+++ src/components/layout/header/header.vue
@@ -5,9 +5,7 @@
       <div class="header__wrapper">
         <a href="/" class="header__logo">
           <div class="header__logo_img">
-            <svg class="icon chevron-icon">
-              <use href="/sprite.svg#logo"></use>
-            </svg>
+            <img src="../../../../public/header/Logo.svg" alt="logo">
           </div>
           <div class="header__logo_text">
             <h1>WeatherCheck</h1>
