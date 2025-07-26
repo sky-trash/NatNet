44a7773af3d09c421408b96f778ae75d395bbac0
Евгений <skytrash@Mac.local>
2025-07-26 15:31:19 +0400

    Initial commit

---
+++ src/components/back/back.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+<template>
+  <div class="back">
+    <a href="/">
+      <div class="back__left">
+      <div class="back__left_img">
+        <img src="../../../public/bookmark/chevron_big_left.svg" alt="">
+      </div>
+      <div class="back__left_text">
+        <h1>Назад</h1>
+      </div>
+    </div>
+    </a>
+    <div class="back__right">
+      <img src="../../../public/bookmark/Bookmark_back.svg" alt="">
+    </div>
+  </div> 
+</template>
+<style lang="scss" scoped>
+  @import "./back.scss";
+</style>
---
+++ src/components/layout/header/header.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__wrapper">
+        <a href="/" class="header__logo">
+          <div class="header__logo_img">
+            <img src="../../../../public/header/Logo.svg" alt="logo">
+            <!-- Можно сделать через <i class="Ic_logo"></i> -->
+          </div>
+          <div class="header__logo_text">
+            <h1>WeatherCheck</h1>
+          </div>
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../../../assets/iconmoon/style.css";
+@import "header.scss";
+</style>
